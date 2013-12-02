5e8fd666613f7ee3121874d1b5f194646f619fd5
Bruce Johnson <bjohnson@kobo.com>
2013-12-01 23:05:47 -0500

    Initial commit for the solution

---
+++ GettingStarted/Account.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GettingStarted
+{
+    public class Account
+    {
+        public int DivisionID { get; set; }
+        public int BranchID { get; set; }
+        public int CostCentreID { get; set; }
+
+        public void BeginEdit() { }
+
+        public void EndEdit() { }
+
+        public void EnableValidation() { }
+    }
+}
---
+++ GettingStarted/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GettingStarted
+{
+    public class Customer
+    {
+        public Customer()
+        {
+            Orders = new List<Order>();
+        }
+
+        public string Name { get; set; }
+        public List<Order> Orders { get; set; }
+    }
+
+    public class Order
+    {
+        public string Description { get; set; }
+    }
+}
